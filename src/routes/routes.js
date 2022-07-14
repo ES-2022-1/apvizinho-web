b0f8c8c3837ce6a2018bee7b546649b229f9a97e
arthuralv <arthur.alves@ccc.ufcg.edu.br>
2022-07-13 22:41:22 -0300

    Criando protótipo do criar/editar anúncio

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 import SignUp from "../pages/SignUp";
+import Announcement from "../pages/Announcement";
 import Layout from "./Layout";
 
 const Router = () => {
@@ -11,6 +12,10 @@ const Router = () => {
         <Route
           element={<Layout isPrivate={false} component={<SignUp />} />}
           path="/signup"
+        />
+        <Route
+          element={<Layout isPrivate={true} component={<Announcement />} />}
+          path="/announcement"
         />
       </Routes>
     </BrowserRouter>
