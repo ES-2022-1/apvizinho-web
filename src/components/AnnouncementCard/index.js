d111e7b8e0e4d5b3bedf9a4346050cff821ecd32
Andrielly Lucena <andrielly.lucena@ccc.ufcg.edu.br>
2022-08-07 16:21:42 -0300

    add deactivate and activate buttons

--- src/components/AnnouncementCard/index.js
+++ src/components/AnnouncementCard/index.js
@@ -1,8 +1,20 @@
-import { EditOutlined } from "@ant-design/icons";
+import {
+  EditOutlined,
+  DeleteOutlined,
+  CloseSquareOutlined,
+  CheckSquareOutlined,
+} from "@ant-design/icons";
 import { Card, CardLink } from "./style";
 import { useNavigate } from "react-router-dom";
+import { Popconfirm } from "antd";
 
-const AnnouncementCard = ({ announcement, canEdit }) => {
+const AnnouncementCard = ({
+  announcement,
+  canEdit,
+  onDelete,
+  onActivate,
+  onDeactivate,
+}) => {
   const navigate = useNavigate();
 
   const handleClickEditIcon = (event) => {
@@ -34,7 +46,42 @@ const AnnouncementCard = ({ announcement, canEdit }) => {
         />
       }
       actions={
-        canEdit && [<EditOutlined key="edit" onClick={handleClickEditIcon} />]
+        canEdit && [
+          <EditOutlined key="edit" onClick={handleClickEditIcon} />,
+          <Popconfirm
+            key="delete"
+            title="Tem certeza que deseja excluir o anúncio?"
+            onConfirm={() => onDelete(announcement.id_announcement)}
+            // onCancel={cancel}
+            okText="Sim"
+            cancelText="Não"
+          >
+            <DeleteOutlined />
+          </Popconfirm>,
+          announcement.status === "ACTIVE" ? (
+            <Popconfirm
+              key="deactivate"
+              title="Tem certeza que deseja desativar o anúncio?"
+              onConfirm={() => onDeactivate(announcement.id_announcement)}
+              // onCancel={cancel}
+              okText="Sim"
+              cancelText="Não"
+            >
+              <CloseSquareOutlined />
+            </Popconfirm>
+          ) : (
+            <Popconfirm
+              key="activate"
+              title="Deseja ativar o anúncio?"
+              onConfirm={() => onActivate(announcement.id_announcement)}
+              // onCancel={cancel}
+              okText="Sim"
+              cancelText="Não"
+            >
+              <CheckSquareOutlined />
+            </Popconfirm>
+          ),
+        ]
       }
     >
       <p>{announcement.description}</p>
